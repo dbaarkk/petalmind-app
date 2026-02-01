f8b5f328a9cd71b16885eed08df5cfbc45c05c75
QuickBoost <legend159980@gmail.com>
2026-02-01 12:50:29 +0530

    Upload next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,54 @@
+import type { NextConfig } from "next";
+import path from "node:path";
+import withPWA from "next-pwa";
+
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+    ],
+  },
+  outputFileTracingRoot: path.resolve(__dirname, '../../'),
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  turbopack: {
+    rules: {
+      "*.{jsx,tsx}": {
+        loaders: [LOADER]
+      }
+    }
+  }
+};
+
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "offlineCache",
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+})(nextConfig);
+// Orchids restart: 1767274276022
